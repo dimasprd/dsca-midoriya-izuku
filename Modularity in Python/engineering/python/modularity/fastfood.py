87b900f5e80110c96273e835a583f560296d6b18
dimasprd <76767355+dimasprd@users.noreply.github.com>
2021-12-28 11:11:22 +0700

    Add files via upload

---
+++ Modularity in Python/engineering/python/modularity/fastfood.py
@@ -0,0 +1,11 @@
+from random import choice
+
+fastfood = ["McDonalds", "KFC", "Burger King", "Wendys",
+            "Pizza Hut", "Toby's", "Lawless", "CFC"]
+
+def pick():
+    """Return random fast food place."""
+    return choice(fastfood)
+
+print("This is from fastfood.py")
+print(fastfood)
